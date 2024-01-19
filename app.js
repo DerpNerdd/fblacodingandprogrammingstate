4a348fdd1429be5924bd4360a08835cd545474b7
DerpNerd <asanch317@west-mec.org>
2024-01-18 21:53:01 -0700

    Form and View practice working

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 const uri = "mongodb+srv://DerpNerd:KSvw9xj7J6zXVV2p@fblabizbond.xgvbq7z.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
@@ -11,18 +15,36 @@ const client = new MongoClient(uri, {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); 
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/submit-partner-info', async (req, res) => {
   try {
     await client.connect();
-    const collection = client.db("FBLABizbond").collection("partners"); 
+    const collection = client.db("FBLABizbond").collection("partners");
     await collection.insertOne(req.body);
     res.send("Partner information saved successfully");
   } catch (err) {
     console.error(err);
     res.send("Error saving data");
+  } finally {
+    await client.close();
+  }
+});
+
+app.get('/view-data', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'view-data.html'));
+});
+
+app.get('/get-partner-info', async (req, res) => {
+  try {
+    await client.connect();
+    const collection = client.db("FBLABizbond").collection("partners");
+    const data = await collection.find({}).toArray();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching data");
   } finally {
     await client.close();
   }
