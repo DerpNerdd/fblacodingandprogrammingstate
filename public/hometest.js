f8a56548302097d8601dd568b0868ba2ae0c1fe1
DerpNerd <asanch317@west-mec.org>
2024-02-06 15:15:41 -0700

    Learning JS/CSS

--- public/hometest.js
+++ public/hometest.js
@@ -5,3 +5,29 @@ document.addEventListener('scroll', function() {
     line.style.transform = 'translateY(' + window.pageYOffset + 'px)';
   });
   
+  document.querySelector('.info-card').addEventListener('mousemove', function(e) {
+    const rect = this.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const deltaX = (x - centerX) / 50; // More subtle tilt
+    const deltaY = (y - centerY) / 50; // More subtle tilt
+
+    this.style.transform = `rotateX(${deltaY * -1}deg) rotateY(${deltaX}deg)`;
+
+    // Correctly target the highlight effect element
+    const highlight = this.querySelector('.highlight-effect');
+    highlight.style.opacity = 1;
+    highlight.style.left = `${x}px`;
+    highlight.style.top = `${y}px`;
+    highlight.style.transform = 'translate(-50%, -50%) scale(1)';
+});
+
+document.querySelector('.info-card').addEventListener('mouseleave', function(e) {
+    this.style.transform = 'rotateX(0) rotateY(0)';
+    // Hide the highlight effect correctly
+    const highlight = this.querySelector('.highlight-effect');
+    highlight.style.opacity = 0;
+    highlight.style.transform = 'translate(-50%, -50%) scale(0)';
+});
